1d7fd7d03c136165b6b1dd224ee5ee1bbf0a5cf8
Florian Rinke <git@florianrinke.de>
2024-05-11 18:55:49 +0200

    accept envvars to have prefix SYNCPLAY_

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,29 +2,43 @@
 
 args=""
 
-if [ -n $SALT ]; then
+if [ -n $SYNCPLAY_SALT ]; then
+  args="$args --salt=$SYNCPLAY_SALT"
+elif [ -n $SALT ]; then
   args="$args --salt=$SALT"
 fi
 
-if [ -n "$PORT" ]; then
+if [ -n "$SYNCPLAY_PORT" ]; then
+  args="$args --port=$SYNCPLAY_PORT"
+elif [ -n "$PORT" ]; then
   args="$args --port=$PORT"
 fi
 
-if [ -n "$ISOLATE" ]; then
+if [ -n "$SYNCPLAY_ISOLATE" ]; then
+  args="$args --isolate-rooms"
+elif [ -n "$ISOLATE" ]; then
   args="$args --isolate-rooms"
 fi
 
-if [ -n "$MOTD" ]; then
+if [ -n "$SYNCPLAY_MOTD" ]; then
+  echo "$SYNCPLAY_MOTD" >> /app/syncplay/motd
+elif [ -n "$MOTD" ]; then
   echo "$MOTD" >> /app/syncplay/motd
+fi
+if [ -f /app/syncplay/motd ]; then
   args="$args --motd-file=/app/syncplay/motd"
 fi
 
-if [ -n "$PASSWORD" ]; then
+if [ -n "$SYNCPLAY_PASSWORD" ]; then
+  args="$args --password=$SYNCPLAY_PASSWORD"
+elif [ -n "$PASSWORD" ]; then
   args="$args --password=$PASSWORD"
 fi
 
-if [ -n "$TLS" ]; then
-  args="$args --tls=$TLS"
+if [ -n "$SYNCPLAY_NOREADY" ]; then
+  args="$args --disable-ready"
+elif [ -n "$NOREADY" ]; then
+  args="$args --disable-ready"
 fi
 
 
